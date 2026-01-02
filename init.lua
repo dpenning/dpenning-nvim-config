01c9565b88a8dca7b4a47fcbe5c4bc1a13c0268b
David Pennington <dpenning1@gmail.com>
2026-01-02 09:59:34 -0800

    fix neovide showing up in the background

--- init.lua
+++ init.lua
@@ -2,3 +2,7 @@ require("config.leader")
 require("plugins.lazy")
 require("config.keymaps")
 require("commands.smart_quit")
+
+if vim.g.neovide then
+	require("config.neovide")
+end
---
+++ lua/config/neovide.lua
@@ -0,0 +1,12 @@
+-- Helper function for transparency and blur
+vim.g.neovide_opacity = 0.8
+vim.g.neovide_window_blurred = true 
+
+vim.api.nvim_create_autocmd("UIEnter", {
+  once = true, -- Ensure this only runs once on startup
+  callback = function()
+    -- We use 'System Events' to find any process named 'neovide' (case insensitive)
+    -- and force it to the front. This is more robust than "tell application".
+    vim.cmd("silent !osascript -e 'tell application \"System Events\" to set frontmost of processes whose name contains \"neovide\" to true'")
+  end,
+})
